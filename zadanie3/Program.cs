194d6c2fa5db9952c5f1490860dfb88eeccdbbfb
skwdebski <skwdebski@gmail.com>
2019-12-18 22:42:58 +0100

    changes in Program

--- zadanie3/Program.cs
+++ zadanie3/Program.cs
@@ -17,49 +17,49 @@
 
             var ratings = matrixProvider.RatingsMatrix;
 
-            float d = 3;
-            float reg = 0.1F; //lambda
+            float dimension = 3;
+            float reg = 0.1F; 
 
-            for (var i = 0; i < 100; i++) // dla 100 powtorzen kroku 3, 4, 5, 6
+            for (var i = 0; i < 100; i++) 
             {
 
-                for (int u = 0; u < ratings.GetLength(0); u++)
-                { // dla kazdego uzytkownika
+                for (int userIndex = 0; userIndex < ratings.GetLength(0); userIndex++)
+                { 
 
-                    var I_u = alsObject.FlatNoZeroOnRow(u, ratings);
+                    var I_u = alsObject.FlatNoZeroOnRow(userIndex, ratings);
 
-                    var P_I_u = alsObject.TakeIndexValues(I_u, matrixP_clone, d);
+                    var P_I_u = alsObject.TakeIndexValues(I_u, matrixP_clone, dimension);
                     var P_I_u_T = Matrix<float>.Transpose(P_I_u);
                    
-                    var E_p = alsObject.CreateEye(d);
+                    var E_p = alsObject.CreateEye(dimension);
 
                     var A_u = Matrix<float>.Summing(Matrix<float>.Multiplication(P_I_u, P_I_u_T), Matrix<float>.Multiplication(E_p, reg)); 
 
-                    var V_u = alsObject.Count_V_u(I_u, P_I_u, ratings, u);
+                    var V_u = alsObject.Count_V_u(I_u, P_I_u, ratings, userIndex);
 
                     var usingMatrix = new Matrix<float>(A_u, V_u);
                     usingMatrix.CalculatePG(A_u, V_u);
                     var solutionOnU = usingMatrix.VectorXGauss;
 
-                    matrixU_clone = alsObject.SwitchGaussColumn(u, matrixU, solutionOnU);
+                    matrixU_clone = alsObject.SwitchGaussColumn(userIndex, matrixU, solutionOnU);
                 }
 
-                for (var p = 0; p < ratings.GetLength(1); p++) // dla kazdego produktu
+                for (var productIndex = 0; productIndex < ratings.GetLength(1); productIndex++) 
                 {
-                    var I_p = alsObject.FlatNoZeroOnColumn(p, ratings);
-                    var U_I_p = alsObject.TakeIndexValues(I_p, matrixU_clone, d);
+                    var I_p = alsObject.FlatNoZeroOnColumn(productIndex, ratings);
+                    var U_I_p = alsObject.TakeIndexValues(I_p, matrixU_clone, dimension);
                      var U_I_p_T = Matrix<float>.Transpose(U_I_p);
-                    var E_u = alsObject.CreateEye(d);
+                    var E_u = alsObject.CreateEye(dimension);
 
                     var B_u = Matrix<float>.Summing(Matrix<float>.Multiplication(U_I_p, U_I_p_T), Matrix<float>.Multiplication(E_u, reg));
 
-                    var W_p = alsObject.Count_W_p(I_p, U_I_p, ratings, p);
+                    var W_p = alsObject.Count_W_p(I_p, U_I_p, ratings, productIndex);
 
                     var usingMatrix = new Matrix<float>(B_u, W_p);
                     usingMatrix.CalculatePG(B_u, W_p);
                     var solutionOnP = usingMatrix.VectorXGauss;
 
-                    matrixP_clone = alsObject.SwitchGaussColumn(p, matrixP, solutionOnP);
+                    matrixP_clone = alsObject.SwitchGaussColumn(productIndex, matrixP, solutionOnP);
                 }
 
             }
