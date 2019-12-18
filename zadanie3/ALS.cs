344a42530c4911f3f2a9b34f4228245ca214952b
skwdebski <skwdebski@gmail.com>
2019-12-18 22:22:32 +0100

    ternary in switchGauss

--- zadanie3/ALS.cs
+++ zadanie3/ALS.cs
@@ -122,15 +122,14 @@ namespace zadanie3
             return W_p;
         }
 
-        public float[,] SwitchGaussColumn(int u, float[,] matrix, float[] GaussResult)
+        public float[,] SwitchGaussColumn(int userIndex, float[,] matrix, float[] GaussResult)
         {
             var ArrayAfterSwitch = new float[matrix.GetLength(0), matrix.GetLength(1)];
             for(var i = 0; i < matrix.GetLength(0); i++)
             {
                 for (var j = 0; j < matrix.GetLength(1); j++)
                 {
-                    if (i == u) ArrayAfterSwitch[i, j] = GaussResult[i];
-                    else ArrayAfterSwitch[i, j] = matrix[i, j];
+                    ArrayAfterSwitch[i, j] = (i == userIndex) ? GaussResult[i] : matrix[i, j];                   
                 }
             }
             return ArrayAfterSwitch;
