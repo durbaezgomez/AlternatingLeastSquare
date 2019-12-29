3eec024bfafb7c8e913e32aa9461dea653c4b393
skwdebski <skwdebski@gmail.com>
2019-12-29 23:24:17 +0100

    deleted comments, cleaned program.cs file

--- zadanie3/Program.cs
+++ zadanie3/Program.cs
@@ -33,71 +33,42 @@ namespace zadanie3
 
                 for (int userIndex = 0; userIndex < ratings.GetLength(0); userIndex++)
                 {
-                    Console.WriteLine("usert index = " + userIndex);
                     var I_u = alsObject.FlatNoZeroOnRow(userIndex, ratings);
-                    Utility<int>.PrintFlatList(I_u);
-
-
                     var P_I_u = alsObject.TakeIndexValues(I_u, matrixP_clone, dimension);
-                    Utility<float>.PrintMatrix(P_I_u);
-
                     var P_I_u_T = Matrix.Transpose(P_I_u);
-                    Utility<float>.PrintMatrix(P_I_u_T);
 
                     var E_p = alsObject.CreateEye(dimension);
-                    Utility<float>.PrintMatrix(E_p);
 
                     var A_u = Matrix.Summing(Matrix.Multiplication(P_I_u, P_I_u_T), Matrix.Multiplication(E_p, reg));
-                    Utility<float>.PrintMatrix(A_u);
 
                     var V_u = alsObject.Count_V_u(I_u, P_I_u_T, ratings, userIndex);
-                    Utility<float>.PrintFlatArray(V_u);
 
                     var usingMatrix = new Matrix(A_u, V_u);
                     usingMatrix.CalculatePG(A_u, V_u);
                     var solutionOnU = usingMatrix.VectorXGauss;
-                    Utility<float>.PrintFlatArray(solutionOnU);
 
                     matrixU_clone = alsObject.SwitchGaussColumn(userIndex, matrixU, solutionOnU);
-                    Utility<float>.PrintMatrix(matrixU_clone);
-                    Console.WriteLine("kolejna petla");
                 }
 
                 for (var productIndex = 0; productIndex < ratings.GetLength(1); productIndex++) 
                 {
-                    Console.WriteLine("product index = " + productIndex);
 
                     var I_p = alsObject.FlatNoZeroOnColumn(productIndex, ratings);
-                    Utility<int>.PrintFlatList(I_p);
-
                     var U_I_p = alsObject.TakeIndexValues(I_p, matrixU_clone, dimension);
-
-                    Utility<float>.PrintMatrix(U_I_p);
-
                     var U_I_p_T = Matrix.Transpose(U_I_p);
-                    Utility<float>.PrintMatrix(U_I_p_T);
 
                     var E_u = alsObject.CreateEye(dimension);
-                    Utility<float>.PrintMatrix(E_u);
 
                     var B_u = Matrix.Summing(Matrix.Multiplication(U_I_p, U_I_p_T), Matrix.Multiplication(E_u, reg));
-                    Utility<float>.PrintMatrix(B_u);
-
 
                     var W_p = alsObject.Count_W_p(I_p, U_I_p_T, ratings, productIndex);
-                    Utility<float>.PrintFlatArray(W_p);
 
                     var usingMatrix = new Matrix(B_u, W_p);
 
-                    var usingMatrix = new Matrix(B_u, W_p);
-                  
                     usingMatrix.CalculatePG(B_u, W_p);
                     var solutionOnP = usingMatrix.VectorXGauss;
 
-                    Utility<float>.PrintFlatArray(solutionOnP);
-
                     matrixP_clone = alsObject.SwitchGaussColumn(productIndex, matrixP, solutionOnP);
-                    Utility<float>.PrintMatrix(matrixP_clone);
                 }
 
             }
